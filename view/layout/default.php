2419ce0334e9f70a07b4069518c70b55e3ec0b08
will <wilfred.varma@gmail.com>
2016-02-07 17:10:58 +0100

    Added header in layout

--- view/layout/default.php
+++ view/layout/default.php
@@ -2,11 +2,16 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
   <head>
     <meta http-equiv="Content-Type" content="test/html; charset=utf-8"/>
-    <title><?php echo $pageTitle; ?></title>
+    <title><?php echo $page_title; ?></title>
     <link rel="stylesheet" href="/savigny-kata/webroot/css/default.css"/>
-    <link href='https://fonts.googleapis.com/css?family=PT+Serif&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=PT+Serif' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
   </head>
   <body>
+    <header>
+      <h1>Karaté - Tai-Chi-Chuan</h1>
+      <h2>Savigny-le-Temple</h2>
+    </header>
     <div>
       <?php echo $content_for_layout; ?>
     </div>
