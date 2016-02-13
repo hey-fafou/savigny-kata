e741ba6619bc84332da503f3b23032ae3303397c
will <wilfred.varma@gmail.com>
2016-02-13 13:51:56 +0100

    Add file hanlding models and database

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+class Config {
+
+  static $debug = 1;
+
+  static $databases = array(
+    'default' => array(
+      'host'      =>  'localhost',
+      'database'  =>  'savigny-kata',
+      'login'     =>  'root',
+      'password'  =>  '')
+    );    
+}
---
+++ core/Model.php
@@ -0,0 +1,39 @@
+<?php
+class Model {
+
+  static $connections = array();
+
+  public $_dbName = 'default';
+  public $_table = false;
+  public $_db;
+
+  function __construct() {
+    $dbConfig = Config::$databases[$this->_dbName];
+    if (isset(Model::$connections[$this->_dbName])) {
+      $this->_db = Model::$connections[$this->_dbName];
+      return true;
+    }
+    try {
+      $pdo = new PDO('mysql:host='.$dbConfig['host'].'; dbname='.$dbConfig['database'].';', $dbConfig['login'], $dbConfig['password']);
+      Model::$connections[$this->_dbName] = $pdo;
+     $this->_db = $pdo; 
+    } catch(PDOException $e) {
+      if (Config::debug >= 1) {
+        die($e->getMessage());
+      } else {
+        die('Unable to connect database.');
+      }
+    }
+
+    if ($this->_table === false) {
+      $this->_table = strtolower(str_replace("Model", "", get_class($this)));
+    }
+  }
+
+  public function find($param) {
+    $sql = 'SELECT * FROM '.$this->_table.'';
+    $rq = $this->_db->prepare($sql);
+    $rq->execute();
+    return $rq->fetchAll(PDO::FETCH_OBJ);
+  }
+}
