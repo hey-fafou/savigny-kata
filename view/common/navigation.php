2f4eb88277005b0e8b1a9f5654b9bcb127b9a26f
will <wilfred.varma@gmail.com>
2016-03-05 11:08:49 +0100

    Forgot to commit menu file

---
+++ view/common/navigation.php
@@ -0,0 +1,14 @@
+<nav>
+  <button type="button" role="button" class="nav-button" aria-hidden="true">
+    <span class="nav-lines"></span>
+    menu
+  </button>
+  <ul>
+    <li><a href="#">Les news</a></li>
+    <li><a href="#">Les photos</a></li>
+    <li><a href="#">Les vidéos</a></li>
+    <li><a href="#">Le blog</a></li>
+    <li><a href="#">Les trucs utiles</a></li>
+    <li><a href="#">Le club</a></li>
+  </ul>
+</nav>
