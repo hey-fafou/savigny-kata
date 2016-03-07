1e05533d93547d710f18257496b37781842b0933
will <wilfred.varma@gmail.com>
2016-03-07 22:49:33 +0100

    changed URL for accessing particular news post

--- view/home/index.php
+++ view/home/index.php
@@ -3,10 +3,10 @@
 <div>
   <h1>Les dernières actualités</h1>
 </div>
-<?php foreach ($posts as $k => $v) { ?>
+<?php foreach ($news_posts as $k => $v) { ?>
   <div>
     <h2><?php echo $v->title; ?></h2>
     <p><?php echo $v->content; ?></p>
-    <p><a href="<?php echo BASE_URL.'/pages/home/'.$v->id;?>"</a>Lire la suite &rarr;</p>
+    <p><a href="<?php echo BASE_URL.'/home/view/'.$v->id;?>"</a>Lire la suite &rarr;</p>
   </div>  
 <?php } ?>
