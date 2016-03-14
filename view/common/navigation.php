2b1e04d7151aa582c5bd973de626a19e7ea471c0
will <wilfred.varma@gmail.com>
2016-03-14 22:16:31 +0100

    Changing color depending on day of the week

--- view/common/navigation.php
+++ view/common/navigation.php
@@ -4,11 +4,11 @@
     menu
   </button>
   <ul>
-    <li><a href="#">Les news</a></li>
-    <li><a href="#">Les photos</a></li>
-    <li><a href="#">Les vidéos</a></li>
-    <li><a href="#">Le blog</a></li>
-    <li><a href="#">Les trucs utiles</a></li>
-    <li><a href="#">Le club</a></li>
+    <li><a href="#" class="nav-link">Les news</a></li>
+    <li><a href="#" class="nav-link">Les photos</a></li>
+    <li><a href="#" class="nav-link">Les vidéos</a></li>
+    <li><a href="#" class="nav-link">Le blog</a></li>
+    <li><a href="#" class="nav-link">Les trucs utiles</a></li>
+    <li><a href="#" class="nav-link">Le club</a></li>
   </ul>
 </nav>
--- webroot/js/mine.js
+++ webroot/js/mine.js
@@ -6,4 +6,22 @@ $(document).ready(function() {
       $(this).addClass("active");
     }
   });
+
+  $(".nav-link").attr('class', function() {
+    if (Date().substring(0,3) == "Mon") {
+      $(this).addClass("violet");
+    } else if (Date().substring(0,3) == "Tue") {
+      $(this).addClass("indigo");
+    } else if (Date().substring(0,3) == "Wed") {
+      $(this).addClass("blue");
+    } else if (Date().substring(0,3) == "Thu") {
+      $(this).addClass("green");
+    } else if (Date().substring(0,3) == "Fri") {
+      $(this).addClass("yellow");
+    } else if (Date().substring(0,3) == "Sat") {
+      $(this).addClass("orange");
+    } else if (Date().substring(0,3) == "Sun") {
+      $(this).addClass("red");
+    }
+  });
 });
