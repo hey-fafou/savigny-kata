8e9041c50486fa1cdc48f3b769344e94c3897300
will <wilfred.varma@gmail.com>
2016-03-14 23:05:59 +0100

    Formatting website with side-note and wrapping body. Need to put that common component in the layout

---
+++ view/common/note.php
@@ -0,0 +1,15 @@
+<div class="note">
+  <div class="Adresse">
+    <h2>Adresse</h2>
+    Gymnase Les Régalles<br/>
+    Rue des oiseaux<br/>
+    77176 - Savigny-le-Temple<br/>
+  </div>
+    <h2>Liens</h2>
+      <ul>
+        <li><a href = "http://www.ffkama.fr/">La FFKAMA</a></li>
+        <li><a href = "http://www.ffkarate.fr/seineetmarne/">La ligue 77</a></li>
+        <li><a href = "http://http://www.aspsavigny.fr/">L'ASPS</a></li>
+      </ul>
+  </div>
+</div>
--- view/home/index.php
+++ view/home/index.php
@@ -1,12 +1,15 @@
 <?php include(ROOT.DS."view/common/header.php"); ?>
+<?php include(ROOT.DS."view/common/note.php"); ?>
 
-<div>
-  <h1>Les dernières actualités</h1>
-</div>
-<?php foreach ($news_posts as $k => $v) { ?>
+<div class="body-wrapper">
   <div>
-    <h2><?php echo $v->title; ?></h2>
-    <p><?php echo $v->content; ?></p>
-    <p><a href="<?php echo BASE_URL.'/home/view/'.$v->id;?>"</a>Lire la suite &rarr;</p>
-  </div>  
-<?php } ?>
+    <h1>Les dernières actualités</h1>
+  </div>
+    <?php foreach ($news_posts as $k => $v) { ?>
+      <div>
+        <h2><?php echo $v->title; ?></h2>
+        <p><?php echo $v->content; ?></p>
+        <p><a href="<?php echo BASE_URL.'/home/view/'.$v->id;?>"</a>Lire la suite &rarr;</p>
+      </div>  
+    <?php } ?>
+</div>
