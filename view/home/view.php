5f7bbdffe74ab3a68737b0180aa7a1b13b1320b3
will <wilfred.varma@gmail.com>
2016-04-03 15:34:26 +0200

    Deleted overflow pushed downward on the left block

--- view/home/view.php
+++ view/home/view.php
@@ -1,8 +1,10 @@
-<?php $page_title = $news_post->title; ?>
-
-<h2>
-  <?php echo $news_post->title; ?>
-  <span class="date"><?php echo $news_post->date_fr;?></span>
-</h2>
-
-<?php echo $news_post->content; ?>
+<div class="body-wrapper">
+  <?php $page_title = $news_post->title; ?>
+  
+  <h2>
+    <?php echo $news_post->title; ?>
+    <span class="date"><?php echo $news_post->date_fr;?></span>
+  </h2>
+  
+  <?php echo $news_post->content; ?>
+</div>
