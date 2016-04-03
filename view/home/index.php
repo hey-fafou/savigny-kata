42a6f11750b58b8d148a61082fb4aeb581e211b1
will <wilfred.varma@gmail.com>
2016-04-03 15:27:48 +0200

    Changed date printing format

--- view/home/index.php
+++ view/home/index.php
@@ -4,7 +4,10 @@
   </div>
     <?php foreach ($news_posts as $k => $v) { ?>
       <div class="home-news-post">
-        <h2 class="post-title"><?php echo "-".$v->date."- ".$v->title; ?></h2>
+        <h2 class="post-title">
+          <?php echo $v->title; ?>
+          <span class="date"><?php echo $v->date_fr;?></span>
+        </h2>
         <p class="post-content"><?php echo $v->content; ?></p>
         <p class="read-more"><a href="<?php echo BASE_URL.'/home/view/'.$v->id;?>"</a>Lire la suite &rarr;</p>
       </div> 
--- view/home/view.php
+++ view/home/view.php
@@ -1,5 +1,8 @@
 <?php $page_title = $news_post->title; ?>
 
-<h1><?php echo $news_post->title; ?></h1>
+<h2>
+  <?php echo $news_post->title; ?>
+  <span class="date"><?php echo $news_post->date_fr;?></span>
+</h2>
 
 <?php echo $news_post->content; ?>
