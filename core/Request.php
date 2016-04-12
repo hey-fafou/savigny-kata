a14ce52fd54dd60d99320ffc3d8bb8b10b59c96a
will <wilfred.varma@gmail.com>
2016-04-12 23:08:28 +0200

    Reforming url by checking Router::routes

--- core/Request.php
+++ core/Request.php
@@ -5,20 +5,15 @@
  */
 class Request {
 
-  public $_url;  // URL called by user
+  public $_url = "";  // URL called by user
   public $_page = 1;
-  
+
   /**
    * Ctor
-   * Initialize _url with user URL if $_SERVER['PATH_INFO'] is set
-   * otherwise _url is left empty ""
+   * Initialize _url with routes created in config.php
    */
   function __construct() {
-    if (!empty($_SERVER['PATH_INFO'])) {
-      $this->_url = $_SERVER['PATH_INFO'];
-    } else {
-      $this->_url = "";
-    }
+    // Each pages can have pagination.
     if (isset($_GET['page'])) {
       if (is_numeric($_GET['page'])) {
         if ($_GET['page'] > 0) {
@@ -26,6 +21,29 @@ class Request {
         }
       }
     }
+
+    $controller = "home";
+    $action = "index";
+    $params = "";
+
+    // Controller is specified
+    if (count(Router::$routes) >= 2) {
+      // Update controller
+      $controller = Router::$routes[1];
+    }
+
+    // Action is also specified
+    if (count(Router::$routes) >= 3) {
+      // Update action
+      $action = Router::$routes[2];
+    }
+
+    // Parameters are given
+    if (count(Router::$routes) == 4) {
+      // Update parameters
+      $params = Router::$routes[3];
+    }
+    $this->_url = $controller.'/'.$action.'/'.$params;     
   }
 
   /**
