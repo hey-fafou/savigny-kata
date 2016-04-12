18dec90fe81325d2dca2cc61fe7b6b56283e4ff2
will <wilfred.varma@gmail.com>
2016-04-12 22:14:27 +0200

    Needs Router included before config for Router::connect

--- core/includes.php
+++ core/includes.php
@@ -1,6 +1,9 @@
 <?php
+require CORE.DS.'Router.php';
+
 require ROOT.DS.'config'.DS.'config.php';
 
+require CORE.DS.'Request.php';
 require CORE.DS.'Controller.php';
 require CORE.DS.'Model.php';
 require CORE.DS.'Dispatcher.php';
