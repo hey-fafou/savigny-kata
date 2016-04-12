1bbc06921f4672f9e647158ad41b5054ca310b92
will <wilfred.varma@gmail.com>
2016-04-12 23:07:56 +0200

    Function connect that creates the routes depending on patterns

--- core/Router.php
+++ core/Router.php
@@ -4,7 +4,8 @@
  * Global class Router that won't be instanciated.
  */
 class Router {
-  
+  static $routes = array();
+
   /**
    * Parse a given url
    * @param [in] $url url to be parsed
@@ -16,9 +17,20 @@ class Router {
 
     // Adding first in $request the controller and the action
     $request->controller = $args[0];
-    $request->action = (!empty($args[1])) ? ($args[1]) : ('index');
+    $request->action = $args[1];
 
     // Then adding other parameters
     $request->args = array_slice($args, 2);
+  }
+
+  static function connect($pattern, &$url) {
+    $r = array();
+
+    $pattern = '/'.str_replace('/', '\/', $pattern).'/';
+
+    if (preg_match($pattern, $url, $match)) {
+      self::$routes[] = $match[1];
+      $url = trim(str_replace($match[1], '', $url), '/');
+    } 
   }
 } 
