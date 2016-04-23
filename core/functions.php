476faf1adcd4d4115e121ce6a6135809cd90f9c3
will <wilfred.varma@gmail.com>
2016-04-23 12:01:11 +0200

    Print object

--- core/functions.php
+++ core/functions.php
@@ -8,11 +8,7 @@ function debug($var, $file = "", $line = 0) {
     echo "<strong>".$file."(".$line."):</strong>";
   }
   echo "<pre style='font-weight:bold;'>";
-  if (is_array($var)) {
     print_r($var);
-  } else {
-    echo $var;
-  }
   echo "</pre>";
 }
 
