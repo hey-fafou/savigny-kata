8bb6d128b95fecfc7f47cc159f6d0a2628a6e252
will <wilfred.varma@gmail.com>
2016-04-26 20:36:33 +0200

    Admin edit view

---
+++ view/home/admin_edit.php
@@ -0,0 +1,20 @@
+<div class="body-wrapper">
+  <div>
+    <h1>Editer un article</h1>
+  </div>
+  <form action="<?php echo BASE_URL.'/'.array_search('admin', Router::$prefixes).'/home/index' ?>" method="post">
+    <div class="input-wrapper">
+      <label for="inputTitle">Titre</label>
+      <div class = "input">
+        <input type="text" id="inputTitle" name="title" value="<?php echo $news_post->title; ?>"/>
+      </div>
+      <label for="inputText">Contenu</label>
+      <div class = "input">
+        <textarea id="inputText" name="content" rows="5" cols="50"><?php echo $news_post->content; ?></textarea>
+      </div>
+      <div class="actions">
+        <input type="submit" class="submit-btn" value="Envoyer"/>
+      </div>
+    </div>
+  </form>
+</div>
