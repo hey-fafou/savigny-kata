fe26c8287730afaf6d0ee48e08eb6391a0622838
will <wilfred.varma@gmail.com>
2016-09-01 22:23:56 +0200

    Added image for social networks

--- view/links/aside.php
+++ view/links/aside.php
@@ -6,19 +6,26 @@
     Rue des oiseaux<br/>
     77176 - Savigny-le-Temple<br/>
   </div>
+  <div class="social-networks">
+    <h3>Réseaux sociaux</h3>
+      <?php foreach ($networks as $k => $v) { 
+              if ($v->title == "Facebook") {
+                echo "<a href = \"".$v->link."\">
+                         <img src=\"".BASE_URL.'/webroot/img/icons/facebook.png'."\" alt=\"\" title=\"Page Facebook\"/>
+                      </a> ";
+              }
+              if ($v->title == "Youtube") {
+                echo "<a href = \"".$v->link."\">
+                          <img src=\"".BASE_URL.'/webroot/img/icons/youtube.png'."\" alt=\"\" title=\"Chaine Youtube\"/>
+                      </a>";
+              }
+        } ?>
+  </div>
   <div class="documents">
     <h3>Documents</h3>
       <ul>
       <?php foreach ($documents as $k => $v) { 
             echo "<li><a href = \"".$v->link."\" class=\"blue-link\">".$v->title."</a></li>";
-        } ?>
-      </ul>
-  </div>
-  <div class="social-networks">
-    <h3>Réseaux sociaux</h3>
-      <ul>
-      <?php foreach ($networks as $k => $v) { 
-        echo "<li><a href = \"".$v->link."\" class=\"blue-link\">".$v->title."</a></li>";
         } ?>
       </ul>
   </div>
