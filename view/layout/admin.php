586b0190c1c3827da4439102039a1aba87ac1380
will <wilfred.varma@gmail.com>
2016-09-05 08:51:34 +0200

    Suppressed paragraph block on line break and put list type to bullet

--- view/layout/admin.php
+++ view/layout/admin.php
@@ -14,6 +14,9 @@
   <script>
   tinymce.init({
     selector: ".wysiwyg",
+    force_br_newlines : true,
+    force_p_newlines : false,
+    forced_root_block : '' // Needed for 3.x
   });
   </script>
   </head>
