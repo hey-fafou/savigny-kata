57674db1b67336e1613fab8af62418bf1ec08340
will <wilfred.varma@gmail.com>
2016-09-06 23:14:42 +0200

    Added club page

---
+++ controller/ClubController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * class ClubController
+ * @brief handles controller for posts
+ */
+class ClubController extends Controller {
+  /**
+   * Loads appropriate model and request to database
+   * for rendering club page.
+   */
+  function index() {
+  }
+}
---
+++ view/club/index.php
@@ -0,0 +1,34 @@
+<div class="no-admin body-wrapper">
+  <h3>Notre club</h3>
+  <p>
+    Notre club a été fondé sur le karaté shotokan en 1981 et a pris son essor il y a 35 ans avec l'arrivée du professeur 
+    Abdé JOUDANI Brevet d'Etat, 5ème dan, compétiteur au niveau Français, Européen et mondial.
+  </p>
+  <p>
+    Nous comptons chaque année près de 200 adhérents de la catégorie poussin (5 ans) à celle de vétéran (pas de limite d'âge). 
+    Parmis nos adhérents karatéka, se trouvent de nombreuses ceintures noires formées par notre Sensei, qui encadrent et partagent
+    leur expérience.
+  </p>
+  <p>
+    Notre Sensei à poursuivi son apprentissage des arts martiaux en s’ouvrant à des disciplines énergétiques 
+    telles que le Tai Chi Chuan, le Qi Qonq et le stretching dynamique.
+    C’est à travers son expérience, ses différents stages et rencontres qu'il nous fait découvrir ces enseignements.  
+  </p>
+
+  <h3>Notre Sensei</h3>
+  <div class = "sensei">
+    <img src="<?php echo BASE_URL.'/webroot/img/club/karate.jpg' ?>" alt = "" title = "Karaté"/>
+    <img src="<?php echo BASE_URL.'/webroot/img/club/stretching.jpg' ?>" alt = "" title = "Stretching dynamique"/>
+    <img src="<?php echo BASE_URL.'/webroot/img/club/taichi.jpg' ?>" alt = "" title = "Taï-chi-chuan"/>
+    <p>
+      Photos de notre Sensei enseignant le Karaté, le stretching dynamique et le Taï-chi-chuan.
+    </p>
+  </div>
+  
+  <h3>L'adresse du club</h3>
+  <div class="address">
+    Gymnase Les Régalles<br/>
+    Rue des oiseaux<br/>
+    77176 - Savigny-le-Temple<br/>
+  </div>
+<div>
